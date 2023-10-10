2d403fb95832404c3a0f5df3e59f17c3160cbbe4
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-10 18:59:01 +0000

    added list update

--- lab3-data/src/app/home/home.page.ts
+++ lab3-data/src/app/home/home.page.ts
@@ -24,6 +24,14 @@ export class HomePage {
 
   async setItem() {
     await this.storage.set(this.key, this.value);
+    const index = this.items.findIndex(item => item.key === this.key);
+    if (index !== -1) {
+      this.items[index].value = this.value;
+    } else {
+      this.items.push({ key: this.key, value: this.value });
+    }
+    this.key = '';
+    this.value = '';
   }
 
   async getItem() {
@@ -35,5 +43,10 @@ export class HomePage {
   async removeItem() {
     await this.storage.remove(this.removeItemKey);
     this.items = this.items.filter(item => item.key !== this.removeItemKey);
+  }
+
+  async removeListItem(item: { key: string, value: string }) {
+    await this.storage.remove(item.key);
+    this.items = this.items.filter(i => i !== item);
   }
 }
