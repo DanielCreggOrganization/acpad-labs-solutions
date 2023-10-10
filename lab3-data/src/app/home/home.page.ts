ac1a95f664eba0ae4938b539c6e52f8e0bce4b8a
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-10 15:57:25 +0000

    Add Ionic Storage template

---
+++ lab3-data/src/app/home/home.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  items: { key: string, value: string }[] = [];
+  key = ''; // initialize key property
+  value = ''; // initialize value property
+  getItemKey = ''; // initialize getItemKey property
+  itemValue = ''; // initialize itemValue property
+  removeItemKey = ''; // initialize removeItemKey property
+
+  constructor(private storage: Storage) {
+    this.initStorage();
+  }
+
+  async initStorage() {
+    await this.storage.create();
+  }
+
+  async setItem() {
+    await this.storage.set(this.key, this.value);
+  }
+
+  async getItem() {
+    const value = await this.storage.get(this.getItemKey);
+    console.log(value);
+    this.itemValue = value;
+  }
+
+  async removeItem() {
+    await this.storage.remove(this.removeItemKey);
+    this.items = this.items.filter(item => item.key !== this.removeItemKey);
+  }
+}
