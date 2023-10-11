61d28a73bb3f6d10263a9ca14d6441b8237d318b
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-11 14:36:18 +0000

    Add login auth and firestore

--- lab3-data/src/app/app.module.ts
+++ lab3-data/src/app/app.module.ts
@@ -8,13 +8,22 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    IonicStorageModule.forRoot()],
+    IonicStorageModule.forRoot(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- lab3-data/src/environments/environment.ts
+++ lab3-data/src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fireappconnect',
+    appId: '1:888029921785:web:fd6810a6ae23e51fc6bd28',
+    storageBucket: 'fireappconnect.appspot.com',
+    apiKey: 'AIzaSyCxSecdaAZceVhGXUuGIGvfsKSfnehm8w0',
+    authDomain: 'fireappconnect.firebaseapp.com',
+    messagingSenderId: '888029921785',
+  },
   production: false
 };
 
