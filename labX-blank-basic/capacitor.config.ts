6761df83059a850e9e0b6a3ac0db10e379bcbae6
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-16 21:45:57 +0000

    Building most basic app

---
+++ labX-blank-basic/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'labX-blank-basic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ labX-blank-basic/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<p>Hello, World!</p>',
+  standalone: true,
+})
+export class AppComponent {}
