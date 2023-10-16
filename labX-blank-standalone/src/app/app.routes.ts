ea6d3b5296b58a063f32e76c657e2e44df4dc2e9
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-16 16:26:36 +0000

    Add a standalone componet

--- labX-blank-standalone/src/app/app.routes.ts
+++ labX-blank-standalone/src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'standalone',
+      loadComponent: () => import('./standalone/standalone.component').then(mod => mod.StandaloneComponent)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
