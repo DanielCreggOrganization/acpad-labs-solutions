f8328877a2d31bef9964fc518f505d804cae2661
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-17 15:00:38 +0000

    Add a button to the ngmodule component

--- lab5-ngmodules/src/app/ngmodule/ngmodule.module.ts
+++ lab5-ngmodules/src/app/ngmodule/ngmodule.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular'; // <-- import IonicModule
 
 import { NgmoduleRoutingModule } from './ngmodule-routing.module';
+import { NgmoduleComponent } from './ngmodule.component'; // <-- import NgmoduleComponent
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NgmoduleComponent], // <-- declare NgmoduleComponent
   imports: [
     CommonModule,
-    NgmoduleRoutingModule
+    NgmoduleRoutingModule,
+    IonicModule // <-- add IonicModule
   ]
 })
 export class NgmoduleModule { }
