f5d72b6cf496811aa51601138a1956175b95430c
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-17 14:48:57 +0000

    Add ngmodule component

---
+++ lab5-ngmodules/src/app/ngmodule/ngmodule.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngmodule',
+  templateUrl: './ngmodule.component.html',
+  styleUrls: ['./ngmodule.component.scss'],
+})
+export class NgmoduleComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ lab5-ngmodules/src/app/ngmodule/ngmodule.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgmoduleRoutingModule } from './ngmodule-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgmoduleRoutingModule
+  ]
+})
+export class NgmoduleModule { }
