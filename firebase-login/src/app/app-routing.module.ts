07c9d53cd062310df953f00a5c045daa1aae7adf
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-21 07:48:52 +0000

    Add comments

--- firebase-login/src/app/app-routing.module.ts
+++ firebase-login/src/app/app-routing.module.ts
@@ -14,13 +14,14 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./pages/login/login.module').then((m) => m.LoginPageModule),
-    ...canActivate(redirectLoggedInToApp),
+    ...canActivate(redirectLoggedInToApp), // Automatically redirect to home page if user is logged in.
+    // the spread operator ... is used to expand the properties of the object returned by the canActivate function into the route configuration object.
   },
   {
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomePageModule),
-    ...canActivate(redirectUnauthorizedToLogin),
+    ...canActivate(redirectUnauthorizedToLogin), // Automatically redirect to login page if user is not logged in.
   },
 ];
 
