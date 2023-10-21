367b991a2365a0d2c8fb8660049bb94c42b045ab
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-21 07:45:30 +0000

    Portect pages with guards

--- firebase-login/src/app/app-routing.module.ts
+++ firebase-login/src/app/app-routing.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {
+  redirectUnauthorizedTo,
+  redirectLoggedInTo,
+  canActivate,
+} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/']);
+const redirectLoggedInToApp = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./pages/login/login.module').then((m) => m.LoginPageModule)
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToApp),
   },
   {
     path: 'home',
     loadChildren: () =>
-      import('./pages/home/home.module').then((m) => m.HomePageModule)
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
+    ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
