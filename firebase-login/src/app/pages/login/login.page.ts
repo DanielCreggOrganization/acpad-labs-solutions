afb9ac6755a689c03b7d455cb7aff039fde01c2f
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-24 10:26:12 +0000

    Add commentary

--- firebase-login/src/app/pages/login/login.page.ts
+++ firebase-login/src/app/pages/login/login.page.ts
@@ -11,18 +11,18 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginPage {
   // Define the form group with the FormBuilder service. The form group is a collection of form controls.
-  // The form controls are the individual fields of the form. The form group is used to track the value and validity state of the form controls.
-  // credentials is the name of the form group.
+  // The form controls are the individual fields of the form. The form group is used to track the value and
+  // validity state of the form controls. credentials is the name of the form group.
   credentials = this.fb.nonNullable.group({
     email: ['daniel.cregg@atu.ie', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
   constructor(
-    private fb: FormBuilder, // <-- Inject the FormBuilder to handle form validation
-    private loadingController: LoadingController, // <-- Inject the LoadingController to handle loading state
-    private alertController: AlertController, // <-- Inject the AlertController to handle errors
-    private authService: AuthService, // <-- Inject the AuthService to handle login
+    private fb: FormBuilder, // <-- Inject the FormBuilder to handle form validation in reactive forms
+    private loadingController: LoadingController, // <-- Inject the LoadingController to handle loading state by displaying a spinner
+    private alertController: AlertController, // <-- Inject the AlertController to handle errors and display alert messages
+    private authService: AuthService, // <-- Inject the AuthService to handle login and registration
     private router: Router // <-- Inject the Router to redirect after successful login
   ) {}
 
@@ -61,6 +61,7 @@ export class LoginPage {
     }
   }
 
+  // The login function is called when the login form is submitted (i.e. the login button is clicked)
   // Login an existing user with the AuthService. If successful, redirect to the home page.
   async login() {
     // Create a loading overlay. This will be displayed while the request is running.
@@ -68,7 +69,6 @@ export class LoginPage {
     await loading.present();
     // Call the login method from the AuthService. This returns a user object if successful, or null if unsuccessful.
     const user = await this.authService.login(this.credentials.getRawValue());
-
     // Log the user object to the console. This will be `null` if the user was not logged in.
     console.log('🚀 ~ file: login.page.ts:73 ~ LoginPage ~ login ~ user', user);
     // Dismiss the loading spinner
@@ -104,7 +104,6 @@ export class LoginPage {
       header,
       message,
       buttons: ['OK'],
-
     });
     await alert.present();
   }
