4a749833f1a410cefc3d6883ffc933fd36675021
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-11-08 10:43:44 +0000

    create project

---
+++ lab6-capacitor/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lab6-capacitor',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
