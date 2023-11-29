fbe90d25ec34e62f21d06b44860654f4b1530d21
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-11-29 10:39:15 +0000

    Add photo loading on page initialization

--- labx-capacitor/src/app/tab2/tab2.page.ts
+++ labx-capacitor/src/app/tab2/tab2.page.ts
@@ -4,14 +4,18 @@ import { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
   constructor(public photoService: PhotoService) {}
-  
+
+  async ngOnInit() {
+    // Retrieve cached photo array data
+    await this.photoService.loadSaved();
+  }
+
   addPhotoToGallery() {
+    // Call the takePicture method from the photoService service
     this.photoService.addNewToGallery();
   }
-  
 }
